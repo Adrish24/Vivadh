dafa686c728b39797d46f17cf98d70317f2ffddf
Adrish24 <130204049+Adrish24@users.noreply.github.com>
2024-07-09 13:50:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,15 +15,7 @@ const port = 5000;
 
 dotenv.config();
 const app = express();
-app.use(
-  cors({
-    origin: ["https://vivadh.vercel.app", "http://localhost:5173/"],
-    methods: ["GET", "POST"],
-    allowedHeaders: [
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/auth", signup);
