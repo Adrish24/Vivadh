4df82018c0b2cb9e653689677a5f874a52406c20
Adrish24 <130204049+Adrish24@users.noreply.github.com>
2024-07-12 11:18:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization', 'multipart/form-data']
-));
+}));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/auth", signup);
